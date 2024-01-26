06c960d6583709be8ce087f064bf528bcfe027f4
kmilo <kamil.kazmierczak@pm.me>
2024-01-26 22:21:30 +0100

    Keymaps modified, pasting improved, git status mapped

--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -1,22 +1,10 @@
--- [[ Basic Keymaps ]]
-
--- Keymaps for better default experience
 -- See `:help vim.keymap.set()`
 vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
-vim.g.mapleader = ' '
-vim.g.maplocalleader = ' '
 
 -- Remap for dealing with word wrap
 vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
 vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
 
--- Diagnostic keymaps
-vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous diagnostic message' })
-vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next diagnostic message' })
-vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Open floating diagnostic message' })
-vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostics list' })
-
--- Moje dodatkowe - centrowanie
 vim.keymap.set('n', 'n','nzz')
 vim.keymap.set('n', 'N','Nzz')
 vim.keymap.set('n', '{', '{zz')
@@ -24,3 +12,7 @@ vim.keymap.set('n', '}', '}zz')
 vim.keymap.set('n', '<C-D>', '<C-D>zz')
 vim.keymap.set('n', '<C-U>', '<C-U>zz')
 vim.keymap.set('n', '<leader>ff', '<cmd>NvimTreeFindFileToggle<cr>', { desc = 'Find file in tree'})
+
+vim.keymap.set("n", 'p', '"0p') -- yanked text is always in "0 register so pasting from that register is good idea
+vim.keymap.set('n', 'P', '"0P')
+vim.keymap.set('n', '<leader>gs', '<cmd>Git<cr>')
