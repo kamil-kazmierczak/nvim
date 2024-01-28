2111664d0cb5723e907edc66ac7d908c0f5e722b
kamilo <kamil.kazmierczak@pm.me>
2024-01-28 17:21:22 +0100

    Modified lualine

--- lua/user/lualine.lua
+++ lua/user/lualine.lua
@@ -7,6 +7,15 @@ local M = {
 			component_separators = '|',
 			section_separators = '',
 		},
+		sections = {
+			lualine_a = { 'mode' },
+			lualine_b = { 'branch', 'diff', 'diagnostics' },
+			lualine_c = { 'filename' },
+			lualine_x = { 'encoding', 'fileformat', 'selectioncount' },
+			-- lualine_y = { 'location' },
+			lualine_y = { },
+			lualine_z = { 'progress' }
+		},
 	},
 }
 
