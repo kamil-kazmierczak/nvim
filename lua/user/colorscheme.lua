a1760d6cd020f9e41f504cf3f9d75c241f41faf2
kmilo <kamil.kazmierczak@pm.me>
2024-01-29 17:59:34 +0100

    Colorscheme changed

--- lua/user/colorscheme.lua
+++ lua/user/colorscheme.lua
@@ -1,11 +1,12 @@
 local M = {
-	"navarasu/onedark.nvim",
+	-- "navarasu/onedark.nvim",
+	'sainnhe/gruvbox-material',
 	priority = 1000,
 	lazy = false
 }
 
 function M.config()
-	vim.cmd.colorscheme "onedark"
+	vim.cmd.colorscheme "gruvbox-material"
 end
 
 return M
