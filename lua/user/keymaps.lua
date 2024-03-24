aba6d092f00580d62a4f8988293947bc56f341ad
kamilo <kamil.kazmierczak@pm.me>
2024-03-24 12:00:16 +0100

    Initial changed mapping to Colemak-DH

--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -2,11 +2,11 @@
 vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
 
 -- Remap for dealing with word wrap
-vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
-vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
+vim.keymap.set('n', 'i', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
+vim.keymap.set('n', 'e', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
 
-vim.keymap.set('n', 'n', 'nzz')
-vim.keymap.set('n', 'N', 'Nzz')
+vim.keymap.set('n', 'h', 'nzz')
+vim.keymap.set('n', 'H', 'Nzz')
 vim.keymap.set('n', '{', '{zz')
 vim.keymap.set('n', '}', '}zz')
 vim.keymap.set('n', '<C-D>', '<C-D>zz')
@@ -19,3 +19,11 @@ vim.keymap.set('v', '<leader>d', [["_d]])
 vim.keymap.set({ 'n', 'v' }, '<leader>y', [["+y]])
 vim.keymap.set('n', '<leader>Y', [["+Y]])
 vim.keymap.set('v', '<leader>p', [["_dP]])
+
+-- Colemak-DH mapping
+vim.keymap.set({ 'n', 'v' }, 'n', 'h')
+vim.keymap.set({ 'n', 'v' }, 'e', 'j')
+vim.keymap.set({ 'n', 'v' }, 'i', 'k')
+vim.keymap.set({ 'n', 'v' }, 'o', 'l')
+vim.keymap.set({ 'n', 'v' }, 'l', 'i')
+vim.keymap.set({ 'n', 'v' }, 'n', 'h')
